c09990ea35cf01dcbb29dc9b29bce1a75a016c22
AdelAcour <adel.achour@outlook.com>
2018-12-01 15:37:34 +0100

    Detail input refactoring

---
+++ app/src/main/java/com/production/achour_ar/qrevent/Models/ContactModel.java
@@ -0,0 +1,14 @@
+package com.production.achour_ar.qrevent.Models;
+
+public class ContactModel {
+
+    private String name;
+    private String firstname;
+    private String telphone;
+    private String email;
+    private String facebook;
+    private String instagram;
+    private String linkedin;
+
+
+}

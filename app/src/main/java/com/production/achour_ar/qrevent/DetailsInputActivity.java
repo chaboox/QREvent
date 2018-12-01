06f6c568e025dad77cafca4a45ad7a484dff0894
AdelAcour <adel.achour@outlook.com>
2018-12-01 14:27:12 +0100

    edit gallery + check if fields are filled

--- app/src/main/java/com/production/achour_ar/qrevent/DetailsInputActivity.java
+++ app/src/main/java/com/production/achour_ar/qrevent/DetailsInputActivity.java
@@ -62,7 +62,26 @@ public class DetailsInputActivity extends AppCompatActivity implements View.OnCl
     }
 
     private void startEvent() {
+        if (areFieldsValid()){
+            //TODO firebase part (Adam farahni)
 
+            //startActivity(new Intent(DetailsInputActivity.this, ));
+        }
+    }
+
+    private boolean areFieldsValid() {
+        boolean valid = true;
+        if (nameET.getText().toString().equals("")){
+            valid = false;
+            nameET.setError(getResources().getText(R.string.fieldrequired));
+            nameET.requestFocus();
+        }
+        if (firstnameET.getText().toString().equals("")){
+            valid = false;
+            firstnameET.setError(getResources().getText(R.string.fieldrequired));
+            firstnameET.requestFocus();
+        }
+        return valid;
     }
 
     @Override
@@ -99,7 +118,7 @@ public class DetailsInputActivity extends AppCompatActivity implements View.OnCl
                 // Set The Bitmap Data To ImageView
                 //profilPicIV.setImageBitmap(selectedBitmap);
                 setProfilePicToImageView(profilePicIV, selectedBitmap);
-                profilePicIV.setScaleType(ImageView.ScaleType.FIT_XY);
+                //profilePicIV.setScaleType(ImageView.ScaleType.FIT_XY);
 
                 //String pathToPic = saveToInternalStorage(selectedBitmap);
 
